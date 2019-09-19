52c72679833bb5f6839a9fd55d9bb0b0c72e521f
career-weevolve <55353796+career-weevolve@users.noreply.github.com>
2019-09-19 23:36:49 +0800

    Create smooth.js

---
+++ smooth.js
@@ -0,0 +1,14 @@
+        $( 'a.lgx-scroll' ).on( 'click', function(event) {
+            var $anchor = $(this);
+            var topTo   = $( $anchor.attr('href') ).offset().top;
+
+            if ( window.innerWidth < 768 ) {
+                topTo = ( topTo - 90 );
+            }
+
+            $( 'html, body' ).stop().animate({
+                scrollTop: topTo
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+            return false;
+        } );
